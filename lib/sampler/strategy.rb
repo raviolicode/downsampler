000ca1b250c4f63522336fb34b898a7af04e1ca0
Lucia Escanellas <raviolicode@gmail.com>
2016-12-12 00:21:09 +0100

    Refactor class method syntax

--- lib/sampler/strategy.rb
+++ lib/sampler/strategy.rb
@@ -1,15 +1,17 @@
 class Strategy
   STRATEGIES = [:peak, :sum]
 
-  def self.peak(samples)
-    samples.max.to_f.round(1)
-  end
+  class << self
+    def peak(samples)
+      samples.max.to_f.round(1)
+    end
 
-  def self.sum(samples)
-    samples.inject(0, :+).to_f.round(1)
-  end
+    def sum(samples)
+      samples.inject(0, :+).to_f.round(1)
+    end
 
-  def self.available_strategies
-    STRATEGIES.map(&:to_s)
+    def available_strategies
+      STRATEGIES.map(&:to_s)
+    end
   end
 end
