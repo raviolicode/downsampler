a1dde317744437dad31129950b4066426240f6f7
Lucia Escanellas <raviolicode@gmail.com>
2016-12-12 15:09:00 +0100

    Refactor processing method

--- lib/sampler/sampler.rb
+++ lib/sampler/sampler.rb
@@ -15,22 +15,25 @@ class Sampler
 
   def process!
     check_valid_source
-    CSV.open(@output, 'w') do |csv|
-      sample_source = []
-      line = 0
+    File.write(@output, obtain_samples)
+  end
+
+  private
+
+  def obtain_samples 
+    sample_source = []
+
+    "".tap do |output|
       CSV.foreach(@input, converters: :numeric) do |row|
-        check_valid_line(row)
-        sample_source << row[0]
-        if( sample_source_complete?(line) )
-          csv << [Strategy.send(@strategy, sample_source)]
+        sample_source << get_value(row)
+        if(sample_source.count == sample_size)
+          output << "#{Strategy.send(@strategy, sample_source)}\n"
           sample_source = []
         end
-        line += 1
       end
     end
   end
 
-  private
   # Count the number of lines using Unix.
   # I'm not opening the file this way
   def total_lines
@@ -41,17 +44,14 @@ class Sampler
     @sample_size ||= total_lines / samples
   end
 
-  def sample_source_complete?(line)
-    (line % sample_size) == (sample_size - 1)
-  end
-
   def check_valid_source
     if((total_lines % samples) != 0)
       raise ArgumentError.new("Source file is invalid, total lines (#{total_lines}) should be multiple of #{samples}.") 
     end
   end
 
-  def check_valid_line(row)
+  def get_value(row)
     raise ArgumentError.new("Input file contains a non-numeric value.") unless row[0].is_a? Numeric 
+    row[0]
   end
 end
